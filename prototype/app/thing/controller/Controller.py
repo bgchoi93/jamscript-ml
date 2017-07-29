2f87461008f9cf71e1f7dd1cc14778af43d24263
Brian Choi <bgchoi93@gmail.com>
2017-07-29 15:18:42 -0400

    removed FogController class from thing package

--- prototype/app/thing/controller/Controller.py
+++
@@ -1,8 +0,0 @@
-#handles assigning neuron computation to different devices
-
-class FogController(object)
-	def __init__(self,network,devices,outputDevice,inputDevice)
-		self.network = network
-		self.devices = devices
-		self.outputDevice = outputDevice
-		self.inputDevice = inputDevice
