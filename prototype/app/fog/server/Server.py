11ef19fd3dc7a2b8712d4ef47f2c39214f888274
Alex Chung <alex.chung@mail.mcgill.ca>
2017-07-31 16:50:50 -0400

    some code

--- prototype/app/fog/server/Server.py
+++ prototype/app/fog/server/Server.py
@@ -1,5 +1,12 @@
+import numpy as numpy
 #use to simulate inputs
 #infinitely running process that sends inputs to different fog controllers 
 #or creates fog controllers when new machine learning requests are needed
 #responsible for requesting weights from the cloud
 
+class Server(object):
+
+	def __init__(self):
+
+	def new_learning_problem(networkString):
+		#load networkString from redis
