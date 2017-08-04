d149a6dcea03c502e7f2d472a1233ac61a0f68f3
Brian Choi <bgchoi93@gmail.com>
2017-08-04 18:24:43 -0400

    added output layer neurons

--- prototype/app/prepare_redis.py
+++ prototype/app/prepare_redis.py
@@ -47,8 +47,52 @@ def main():
     # LAYER 2 : Output layer
     # define neurons of ffnn:l2
     rdb.hset('ffnn:l2', 'neurons', 'ffnn:l2:neurons')
-    rdb.zadd('ffnn:l2:neurons', 'ffnn:l2:n0', 0, 'ffnn:l2:n1', 1)
+    rdb.zadd('ffnn:l2:neurons', 'ffnn:l2:n0', 0, 'ffnn:l2:n1', 1, 'ffnn:l2:n2', 2, 'ffnn:l2:n3', 3)
 
     #  neuron ffnn:l2:n0
+    ffnn_l2_n0 = {
+        'bias': -0.8576133251190186,
+        'weights': 'ffnn:l2:n0:weights'
+    }
+    rdb.hmset('ffnn:l2:n0', ffnn_l2_n0)
+    rdb.rpush(
+        'ffnn:l2:n0:weights',
+        -11.438902854919434,
+        16.214269638061523
+    )
 
     #  neuron ffnn:l2:n1
+    ffnn_l2_n1 = {
+        'bias': -11.27922534942627,
+        'weights': 'ffnn:l2:n0:weights'
+    }
+    rdb.hmset('ffnn:l2:n1', ffnn_l2_n1)
+    rdb.rpush(
+        'ffnn:l2:n1:weights',
+        -10.637711524963379,
+        -17.856063842773438
+    )
+
+    #  neuron ffnn:l2:n2
+    ffnn_l2_n2 = {
+        'bias': 0.848839521408081,
+        'weights': 'ffnn:l2:n2:weights'
+    }
+    rdb.hmset('ffnn:l2:n2', ffnn_l2_n2)
+    rdb.rpush(
+        'ffnn:l2:n0:weights',
+        -6.472053527832031,
+        -3.282294273376465
+    )
+
+    #  neuron ffnn:l2:n3
+    ffnn_l2_n3 = {
+        'bias': -9.703481674194336,
+        'weights': 'ffnn:l2:n0:weights'
+    }
+    rdb.hmset('ffnn:l2:n3', ffnn_l2_n3)
+    rdb.rpush(
+        'ffnn:l2:n3:weights',
+        -38.89588165283203,
+        9.490970611572266
+    )
